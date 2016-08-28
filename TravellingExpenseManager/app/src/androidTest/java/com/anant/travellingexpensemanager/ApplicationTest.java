0df08529b69cd905f9e2579816ec9b620e547bf2
Mayank Deep Singh <mail2mayanksingh@gmail.com>
2016-08-28 13:31:26 +0530

    Initial commit

---
+++ TravellingExpenseManager/app/src/androidTest/java/com/anant/travellingexpensemanager/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.anant.travellingexpensemanager;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
